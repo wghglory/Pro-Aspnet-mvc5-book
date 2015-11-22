0651e1a632b456d3c3281f4fab9a36c20ab8ed54
Guanghui Wang <wghglory89@gmail.com>
2015-11-22 11:09:06 -0800

    mvc 5 book

---
+++ Chapter 16/UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using UrlsAndRoutes.Infrastructure;
+
+namespace UrlsAndRoutes
+{
+    public class RouteConfig
+    {
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+
+            routes.RouteExistingFiles = true;
+
+            routes.MapMvcAttributeRoutes();
+
+            routes.IgnoreRoute("Content/{filename}.html");
+
+            routes.Add(new Route("SayHello", new CustomRouteHandler()));  //localhost:SayHello
+
+            routes.Add(new LegacyRoute(
+                    "~/articles/Windows_3.1_Overview.html",
+                    "~/old/.NET_1.0_Class_Library"));
+
+            routes.MapRoute("MyRoute", "{controller}/{action}", null,
+                new[] { "UrlsAndRoutes.Controllers" });
+
+            routes.MapRoute("MyOtherRoute", "App/{action}", new { controller = "Home" },
+                new[] { "UrlsAndRoutes.Controllers" });
+
+            routes.MapRoute("", "{controller}/{action}", new { controller = "Home", action = "Index" },
+                new[] { "UrlsAndRoutes.Controllers" });
+        }
+    }
+}
---
+++ Chapter 19/ControllerExtensibility/ControllerExtensibility/Controllers/RemoteDataController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using ControllerExtensibility.Models;
+using System.Threading.Tasks;
+
+namespace ControllerExtensibility.Controllers {
+    public class RemoteDataController : Controller {
+
+        //method 1: task in the controller
+        public async Task<ActionResult> Data() {
+            string data = await Task<string>.Factory.StartNew(() => {
+                return new RemoteService().GetRemoteData();
+            });
+
+            return View((object)data);
+        }
+
+        //method 2: task in the GetRemoteDataAsync() method
+        public async Task<ActionResult> ConsumeAsyncMethod() {
+            string data = await new RemoteService().GetRemoteDataAsync();
+            return View("Data", (object)data);
+        }
+    }
+}
